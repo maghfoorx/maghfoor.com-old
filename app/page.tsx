813a79efec9d91303b90e808a8c94bbf5eb728f6
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-25 11:39:27 +0000

    added env variable

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,20 @@ const client = createClient({
   projectId,
   dataset: 'production',
   apiVersion: "2023-03-25",
-  useCdn: true
+  useCdn: false
 })
 
 export default async function Home() {
 
   const postData = await client.fetch(groq`*[_type == "post"]`);
 
-  console.log(postData)
+  console.log(postData[0].title)
 
   return (
     <main>
       <div>
         Hello
+        <p>The title of my blog is: {postData[0].title}</p>
       </div>
     </main>
   )
