310edc59ebf22de778581ff52d46850ad53d9914
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-26 12:14:40 +0100

    created homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,11 @@
 import { createClient, groq } from 'next-sanity'
 
-const projectId = process.env.SANITY_PROJECT_ID
-
-const client = createClient({
-  projectId,
-  dataset: 'production',
-  apiVersion: "2023-03-25",
-  useCdn: false
-})
-
 export default async function Home() {
-
-  const postData = await client.fetch(groq`*[_type == "post"]`);
-
-  console.log(postData[0].title)
 
   return (
     <main>
       <div>
-        Hello
-        <p>The title of my blog is: {postData[0].title}</p>
+        Home Page
       </div>
     </main>
   )
