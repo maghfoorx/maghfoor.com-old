331b38398d50c9a620f5c31beccb48e878055342
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-26 15:29:43 +0100

    created sanity client

---
+++ utils/sanityClient.ts
@@ -0,0 +1,11 @@
+import { createClient } from "next-sanity";
+
+const projectId = process.env.SANITY_PROJECT_ID;
+const dataset = process.env.SANITY_DATASET;
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion: "2023-03-25",
+  useCdn: true,
+});
