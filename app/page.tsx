26055ca8c2b33b6826cde7aa7fb4a6c6db693129
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-31 22:53:18 +0100

    create homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import variables from "../styles/variables.module.scss";
 import Link from 'next/link'
+import "../styles/homepage.scss";
+import Image from "next/image";
+import logo from "../public/logo.png";
 
 export default async function Home() {
 
   return (
     <main>
-      <div>
-        Home Page
-        <br></br>
-        <Link href={"/blog"}>Check out my blog</Link>
-        <p className={variables.title}>testing Sass</p>
+      <div className="home-page">
+        <Image src={logo} alt="logo" className="logo" />
+        <h1>Maghfoor Ahmed</h1>
+        <Link href={"/blog"}>blog</Link>
+
       </div>
     </main>
   )
