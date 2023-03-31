df54d8a9b6e2a81c54df6a71d3839fb85b017cec
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-31 23:20:17 +0100

    created footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import "../styles/footer.scss"
+
+export default function Footer() {
+    return (
+        <section className="footer-comp">
+            <p>made by maghfoor.</p>
+        </section>
+    )
+}
