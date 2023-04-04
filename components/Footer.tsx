3a12db0bf94ea93948db64162817770e52ef02b2
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-04-04 23:29:23 +0100

    changed word

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import "../styles/footer.scss"
 export default function Footer() {
     return (
         <section className="footer-comp">
-            <p>made by maghfoor.</p>
+            <p>assembled by maghfoor.</p>
         </section>
     )
 }
