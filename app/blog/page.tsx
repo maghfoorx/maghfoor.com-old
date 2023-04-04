aede3cd115f8ad0d0d7412bdbcf66a60fb106d9b
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-04-04 19:31:40 +0100

    created navbar compoenent and added more structure to BlogPost

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,10 @@
+import NavBar from "@/components/NavBar";
 import { createClient, groq } from "next-sanity";
 import Link from "next/link";
+import "../../styles/navBar.scss"
+import { urlFor } from "@/components/RichTextComponents";
+import Image from "next/image";
+import "../../styles/blogpage.scss"
 
 const projectId = process.env.SANITY_PROJECT_ID
 
@@ -15,20 +20,27 @@ export default async function Blog() {
 
     return (
         <div>
-            The Blog Page
-            <p>The title of my blog is: {blogs[0].title}</p>
-            {blogs?.map((post: any) => {
-                return <BlogPost key={post._id} blogPost={post} />
-            })}
+            <NavBar />
+            <section className="blogs-section">
+                {blogs?.map((post: any) => {
+                    return <BlogPost key={post._id} blogPost={post} />
+                })}
+            </section>
         </div>
     )
 }
 
 function BlogPost({ blogPost }: any): JSX.Element {
-    console.log(blogPost.slug.current)
+    console.log(blogPost)
     return (
-        <div>
-            <Link href={`/blog/${blogPost.slug.current}`}>Click to go to post</Link>
+        <div className="blog-post-summary">
+            <div className="image-wrapper">
+                <Image src={urlFor(blogPost.mainImage.asset).url()} alt={blogPost.title} fill className="main-image" />
+            </div>
+            <Link href={`/blog/${blogPost.slug.current}`}>
+                <h3>{blogPost.title}</h3>
+            </Link>
+            <p>{new Date(blogPost.publishedAt).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })}</p>
         </div>
     )
 }
