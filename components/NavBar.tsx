e8bf31aba934c2a0d962bcd1cb1a91852f108f94
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-04-04 19:32:06 +0100

    created NavBar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import Link from "next/link"
+import "../styles/navBar.scss"
+type NavBarProps = {
+    href?: string
+}
+export default function NavBar({ href }: NavBarProps) {
+    return (
+        <div>
+            <div className="nav-bar">
+                {href && <Link href={href}>back</Link>}
+                <p className="nav-title">Maghfoor&apos;s Blog</p>
+                <Link href={"/"}><Image src={"/logo.png"} alt="logo" width={70} height={70} className="nav-logo" /></Link>
+            </div>
+        </div>
+    )
+}
