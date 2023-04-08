daec1f2acbdfb9b65e05acdff91de55cfa873eaa
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-04-08 14:33:08 +0100

    added apiVersion

--- utils/sanityClient.ts
+++ utils/sanityClient.ts
@@ -2,10 +2,11 @@ import { createClient } from "next-sanity";
 
 const projectId = process.env.SANITY_PROJECT_ID;
 const dataset = process.env.SANITY_DATASET;
+const apiVersion = process.env.SANITY_API_VERSION;
 
 export const client = createClient({
   projectId,
   dataset,
-  apiVersion: "2023-03-25",
+  apiVersion,
   useCdn: true,
 });
