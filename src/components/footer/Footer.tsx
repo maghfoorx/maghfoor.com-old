17cca10ac3ca1e7e7841f53494ebf919811c0a36
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2024-01-20 12:29:04 +0000

    complete reset

--- src/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./footer.module.scss"
-
-export default function Footer() {
-    return (
-        <section className={styles['footer-comp']}>
-            <p>assembled by maghfoor.</p>
-        </section>
-    )
-}
