2355afa306afe3aed5121a5f0e20215ebc7e15ec
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2024-01-21 21:15:49 +0000

    new content on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,46 @@
-import Link from "next/link";
 import Image from "next/image";
-import logo from "../public/logo.png";
+import codingTournament from "../public/images/coding-tournament.png";
+import starAward from "../public/images/star-award.png";
+
+const highlightsData = [
+  {
+    image: codingTournament,
+    description:
+      "I won a keyboard and a Supercar Experience Day at my first very tech tournament. This was the secure code warrior security tournament held by IAGL.",
+  },
+  {
+    image: starAward,
+    description:
+      "I received a company wide Star Award just after 7 months of working at IAGL. This award is given those who accomplish a lot and make a meaningful difference at the company.",
+  },
+];
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image src={logo} alt="logo" />
-      <h1>Maghfoor Ahmed</h1>
-      <p>maghfoor.dev@gmail.com</p>
-    </main>
+    <section className="text-center mt-4">
+      <div className="flex flex-col gap-4">
+        <h1 className="font-bold text-7xl">HEY! I AM MAGHFOOR</h1>
+        <p>
+          A full stack engineer with a strong passion to continuously improve
+          myself. I have strong experience with building full stack applications
+          from design to deployment.
+        </p>
+        <h2 className="font-bold text-5xl">HIGHLIGHTS</h2>
+        <div className="flex flex-row justify-between gap-4">
+          {highlightsData.map((highlight) => {
+            return (
+              <div key={highlight.image.blurDataURL}>
+                <Image
+                  src={highlight.image}
+                  alt=""
+                  className="object-cover h-[322px] w-[455px] rounded-md"
+                />
+                <p>{highlight.description}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
