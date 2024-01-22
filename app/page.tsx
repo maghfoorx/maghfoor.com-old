0226fc881e470bcf02c71cf4145cc075eeaa27ea
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2024-01-22 18:49:19 +0000

    putting iagl back on

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import HeroSection from "@/src/hero-section";
-import IaglSection from "@/src/iagl-section";
+import HeroSection from "../src/hero-section";
+import IaglSection from "../src/iagl-section";
 
 export default async function Home() {
   return (
     <main className="text-center mt-4 min-w-0">
       <HeroSection />
-      {/* <IaglSection /> */}
+      <IaglSection />
     </main>
   );
 }
