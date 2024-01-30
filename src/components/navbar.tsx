2b03316d2d8cb318e4645c0f33d93a007a54da80
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2024-01-30 19:50:18 +0000

    added links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,12 @@ export default function Navbar() {
   return (
     <nav className="flex flex-row items-center justify-center gap-4 max-w-4xl">
       <Image src={logo} alt="logo" width={50} height={50} className="logo" />
-      {/* <Link href={"#experience"}>Experience</Link> */}
+      <Link href={"https://github.com/maghfoor-dev"} target="_blank">
+        GitHub
+      </Link>
+      <Link href={"https://www.linkedin.com/in/maghfoor/"} target="_blank">
+        LinkedIn
+      </Link>
     </nav>
   );
 }
