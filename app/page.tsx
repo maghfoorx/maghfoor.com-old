03d11a845231ef6dc28250fad229200ab6b39dbd
maghfoor-dev <maghfoor.dev@gmail.com>
2024-03-27 20:52:50 +0000

    added edge runtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Projects from "@/src/projects";
 import HeroSection from "../src/hero-section";
 
+export const runtime = "edge";
+
 export default async function Home() {
   return (
     <main className="text-left mt-4 min-w-0">
