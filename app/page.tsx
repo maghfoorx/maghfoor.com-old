b075fc70d03d47e51976fe19b55497ae75f1bda2
maghfoor-dev <maghfoor.dev@gmail.com>
2024-03-27 20:29:37 +0000

    integrated projects component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Projects from "@/src/projects";
 import HeroSection from "../src/hero-section";
 
 export default async function Home() {
   return (
     <main className="text-left mt-4 min-w-0">
       <HeroSection />
+      <Projects />
     </main>
   );
 }
