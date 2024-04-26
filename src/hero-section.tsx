03f146d8e6d61e85c8129fb9dd29b10f49f7ba0c
maghfoor-dev <maghfoor.dev@gmail.com>
2024-04-26 11:00:22 +0100

    updated hero section

--- src/hero-section.tsx
+++ src/hero-section.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@/components/ui/card";
 import codingTournament from "../public/images/coding-tournament.png";
 import starAward from "../public/images/star-award.png";
 import Image from "next/image";
@@ -5,11 +6,13 @@ import Image from "next/image";
 const highlightsData = [
   {
     image: codingTournament,
+    title: "Won Secure Code Tournament",
     description:
       "I won a keyboard and a Supercar Experience Day at my first very tech tournament. This was the secure code warrior security tournament held by IAGL.",
   },
   {
     image: starAward,
+    title: "Received Company Wide Star Award",
     description:
       "I received a company wide Star Award just after 7 months of working at IAGL. This award is given those who accomplish a lot and make a meaningful difference at the company.",
   },
@@ -25,22 +28,36 @@ export default function HeroSection() {
       </div>
       <div>
         <h2 className="font-bold text-3xl md:text-4xl">HIGHLIGHTS</h2>
-        <div className="">
+        <div className="flex flex-col md:flex-row md:gap-4">
           {highlightsData.map((highlight) => {
             return (
               <div
                 key={highlight.image.blurDataURL}
                 className="flex flex-col md:flex-row gap-2 py-2"
               >
-                <Image
+                {/* <Image
                   src={highlight.image}
                   alt=""
                   width={400}
                   height={300}
                   className="border border-black"
                   loading="lazy"
-                />
-                <p>{highlight.description}</p>
+                /> */}
+                {/* <p>{highlight.description}</p> */}
+                <Card className="max-w-[400px]">
+                  <Image
+                    src={highlight.image}
+                    alt=""
+                    width={400}
+                    height={300}
+                    className="rounded-t-md"
+                    loading="lazy"
+                  />
+                  <div className="p-2">
+                    <p className="font-bold">{highlight.title}</p>
+                    <p>{highlight.description}</p>
+                  </div>
+                </Card>
               </div>
             );
           })}
