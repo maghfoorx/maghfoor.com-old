1fba83fa5c0e723451df6a40e9e505a975f80a36
maghfoor-dev <maghfoor.dev@gmail.com>
2024-04-26 18:22:47 +0100

    fixed footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="flex flex-row gap-5 justify-center items-center p-4 text-sm">
       <p>2024</p>
-      <p className="">made by maghfoor</p>
-      <p className="">thanks for visiting :)</p>
+      <p className="">made by Maghfoor</p>
+      <p className="">thanks for visiting😁</p>
     </footer>
   );
 }
