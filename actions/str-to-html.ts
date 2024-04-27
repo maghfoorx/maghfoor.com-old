838f6b5c4eff875c25397dd752c3bf6973e1a057
maghfoor-dev <maghfoor.dev@gmail.com>
2024-04-27 17:43:59 +0100

    added links to headings

--- actions/str-to-html.ts
+++ actions/str-to-html.ts
@@ -5,6 +5,8 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeSanitize from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
+import rehypeSlug from "rehype-slug";
+import rehypeAutoLinkHeadings from "rehype-autolink-headings";
 
 export async function strToHtml(input: string) {
   const file = await unified()
@@ -12,6 +14,12 @@ export async function strToHtml(input: string) {
     .use(remarkRehype) // Transform to HTML AST
     .use(rehypeSanitize) // Sanitize HTML input
     .use(rehypeStringify) // Convert AST into serialized HTML
+    .use(rehypeSlug)
+    .use(rehypeAutoLinkHeadings, {
+      properties: {
+        className: ["anchor"],
+      },
+    })
     .process(input);
 
   return String(file);
