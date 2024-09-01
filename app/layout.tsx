462d5a09a28f65048b5acd4efb905e72d01b33bc
maghfoor-dev <maghfoor.dev@gmail.com>
2024-09-01 07:40:52 +0100

    redirecting to sphorb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Montserrat, Work_Sans, Ubuntu, Lexend } from "next/font/google";
+import { Lexend } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/src/components/navbar";
-import Script from "next/script";
-import Footer from "@/components/footer";
 
 const montserrat = Lexend({ subsets: ["latin"] });
 
@@ -24,15 +21,7 @@ export default function RootLayout({
       <body
         className={`${montserrat.className} antialiased border-box flex flex-col min-h-[100vh]`}
       >
-        <Navbar />
         {children}
-        <Footer />
-        <Script
-          defer
-          src="https://static.cloudflareinsights.com/beacon.min.js"
-          data-cf-beacon='{"token": "07dbc8f0925840219b52a3152f351e2d"}'
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
-import Projects from "@/src/projects";
-import HeroSection from "../src/hero-section";
-import BlogsSection from "@/components/blogs-section";
-import { getPostsMeta } from "@/actions/get-posts-meta";
+import Link from "next/link";
 
 export const runtime = "edge";
 
 export default async function Home() {
-  const posts = await getPostsMeta();
   return (
-    <main className="min-w-0 text-left">
-      <HeroSection />
-      <Projects />
-      <BlogsSection posts={posts} />
+    <main className="min-w-0 flex flex-col items-center">
+      <section className="border-2 border-black rounded max-w-3xl m-10 p-3 bg-yellow-300 shadow-md mx-4">
+        <h1 className="text-4xl text-center font-semibold">Maghfoor Ahmed</h1>
+        <div className="flex items-center">
+          <Link
+            href={"https://sphorb.io"}
+            className="hover:underline mx-auto text-blue-900"
+          >
+            sphorb.io
+          </Link>
+        </div>
+      </section>
     </main>
   );
 }
